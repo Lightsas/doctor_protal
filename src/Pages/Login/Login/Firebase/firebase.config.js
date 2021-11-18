b740477ba03288ca68a60bc2e7148e636d9555e0
Liton ahmed <litonahmedlitu575@gmail.com>
2021-11-19 02:58:51 +0600

    final doctor protal font-end complet

---
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD_vHx0yO-eM_Q2P3a3RmXGxQjBIHuWCbE",
+    authDomain: "doctors-portal-8d80a.firebaseapp.com",
+    projectId: "doctors-portal-8d80a",
+    storageBucket: "doctors-portal-8d80a.appspot.com",
+    messagingSenderId: "804306055153",
+    appId: "1:804306055153:web:14c85a0668f9f383715e50"
+  };
+export default firebaseConfig;
